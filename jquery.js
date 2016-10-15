7b455a7fd1ebe4634120fd4c3308597e18c2becd
tressacroaker <tressacroaker36@gmail.com>
2016-10-15 03:54:01 -0600

    changed to newspaper

--- jquery.js
+++ jquery.js
@@ -1,7 +1,39 @@
 $(document).ready(function() {
 
-  $(".menu").on("click", function() {
-      $(".mainNav").toggle();
+  var currentIndex = 0,
+    items = $('.container div'),
+    itemAmt = items.length;
+
+  function cycleItems() {
+    var item = $('.container div').eq(currentIndex);
+    items.hide();
+    item.css('display','inline-block');
+  }
+
+  var autoSlide = setInterval(function() {
+    currentIndex += 1;
+    if (currentIndex > itemAmt - 1) {
+      currentIndex = 0;
+    }
+    cycleItems();
+  }, 1000);
+
+  $('.next').click(function() {
+    clearInterval(autoSlide);
+    currentIndex += 1;
+    if (currentIndex > itemAmt - 1) {
+      currentIndex = 0;
+    }
+    cycleItems();
+  });
+
+  $('.prev').click(function() {
+    clearInterval(autoSlide);
+    currentIndex -= 1;
+    if (currentIndex < 0) {
+      currentIndex = itemAmt - 1;
+    }
+    cycleItems();
   });
 
 });
